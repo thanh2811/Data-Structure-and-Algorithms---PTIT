baae61ed438d0622a3db57c71100e372e6c7980e
thanh2811 <toilathanh.2k@gmail.com>
2020-08-07 11:22:10 +0700

    Graph

--- Contest 9/bai20.cpp
+++ Contest 9/bai20.cpp
@@ -4,49 +4,48 @@ using namespace std;
 
 typedef long long ll;
 #define For(i,a,b) for(int i=a;i<=b;i++)
-int n,m,x,y;
 
-void bfs(int u,int s,vector<int> v[],vector<int> &vs){
+vector<int> v[10005];
+int vs[10005];
+
+int bfs(int dt){
+    int u;
+    if(dt==1) u = 2;
+    else u = 1;
+    vs[u] = 1;          // Git pull orgin master
+    vs[dt] = 1;
+    int n = 0;
     queue<int> q;
     q.push(u);
-    while (!q.empty()){
-        int t = q.front();
-        q.pop();
-        vs[t] = 1;
+    while(!q.empty()){
+        int t = q.front(); q.pop();
+        n++;
         for(int i=0;i<v[t].size();i++){
-            if(vs[v[t][i]]==0 && v[t][i]!=s){
-                vs[v[t][i]] = 1;
-                q.push(v[t][i]);
+            int x = v[t][i];
+            if(vs[x]==0){
+                vs[x] = 1;
+                q.push(x);
             }
         }
     }
-    
-}
-
-int Dinh_Tru(int u,vector<int> v[]){
-    vector<int> vs(n+5,0);
-    int d = 0;
-    For(i,1,n){
-        if(i==u) continue;
-        if(vs[i]==0){
-            d++;
-            if(d==2) return 1;
-            bfs(i,u,v,vs);
-        }        
-    }
-    return 0;
+    return n+1;
 }
 
 void Res(){
+    int n,m;
     cin>>n>>m;
-    vector<int> v[10005];
+    For(i,1,n) v[i].clear();
     For(i,1,m){
+        int x,y;
         cin>>x>>y;
         v[x].push_back(y);
         v[y].push_back(x);
     }
     For(i,1,n){
-        if(Dinh_Tru(i,v)) cout<<i<<" ";
+        memset(vs,0,sizeof(vs));
+        if(bfs(i) != n){
+            cout<< i <<" ";
+        }
     }
     cout<<'\n';
 }
@@ -56,6 +55,5 @@ int main(){
     cin>>test;
     while(test--){
         Res();
-        
     }
 }
